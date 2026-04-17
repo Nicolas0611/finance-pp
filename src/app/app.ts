2f2d9d756f8b3fe4afbc27b8cf08b7ff79d4eb89
Nicolás Sierra <nico.sierra13@gmail.com>
2026-04-16 22:10:57 -0500

    delete inital project settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('finance-pp');
+  protected readonly title = signal('finance-app');
 }
